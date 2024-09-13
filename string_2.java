85941d37bb10fcc9f0a06e8692854393024ce901
june0o7 <rajdeeppal6057@gmail.com>
2024-09-13 18:27:32 +0530

    updated version

--- string_2.java
+++ string_2.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.*;
 public class string_2{
     
@@ -18,25 +17,4 @@ public class string_2{
 
 
     }
-=======
-import java.util.*;
-public class string_2{
-    
-    
-    
-    public static void main(String[] args) {
-        int a ;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a string");
-        String s = sc.nextLine();
-        for(int i=0;i<s.length();i++)
-        {
-            int ch = s.charAt(i);
-            System.out.print(" "+ ch );
-        }
-
-
-
-    }
->>>>>>> 0c9ce6c6c091f76f3856d98f64248a270c475b79
 } 
--- string_4.java
+++ string_4.java
@@ -1,25 +1,24 @@
-<<<<<<< HEAD
-import java.util.Scanner;
+import java.util.*;
 
 public class string_4 {
 
      public static void check(String s){
-        int letter=0,up=0,lo=0,dig=0;
+        int letter=0,up=0,lo=0,dig=0,word=0,sp=0,vo=0;
         for(int i = 0 ; i<s.length();i++){
             char ch = s.charAt(i);
             if(Character.isLetter(ch)){  
             letter++;
+            if("AEIOUaeiou".indexOf(ch)!=-1)vo++;
             if(Character.isUpperCase(ch))
             up++;
             if(Character.isLowerCase(ch))
             lo++;
             }
             if(Character.isDigit(ch))dig++;
-
-
-          
-
+            if(ch == ' ') word++;
+            if(!Character.isLetterOrDigit(ch))sp++;
         }
+        System.out.println("alphabate: "+ letter + "\nvowel: "+vo+"\nuppercase: "+up+"\nlowercase: "+lo+"\nword: "+(word+1)+"\ndigit: "+dig+"\nspecial char: "+sp);
 
      }
 
@@ -34,40 +33,3 @@ public class string_4 {
     }
     
 }
-=======
-import java.util.Scanner;
-
-public class string_4 {
-
-     public static void check(String s){
-        int letter=0,up=0,lo=0,dig=0;
-        for(int i = 0 ; i<s.length();i++){
-            char ch = s.charAt(i);
-            if(Character.isLetter(ch)){  
-            letter++;
-            if(Character.isUpperCase(ch))
-            up++;
-            if(Character.isLowerCase(ch))
-            lo++;
-            }
-            if(Character.isDigit(ch))dig++;
-
-
-          
-
-        }
-
-     }
-
-
-
-     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter a string");
-        String s = sc.nextLine();
-        check(s);
-        
-    }
-    
-}
->>>>>>> 0c9ce6c6c091f76f3856d98f64248a270c475b79
