2e0928313979fa8bbaf40267d6c9f23f399ca108
june0o7 <rajdeeppal6057@gmail.com>
2024-09-14 22:40:07 +0530

    done process

---
+++ string_10.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+// In Piglatin a word such as KING becomes INGKAY, TROUBLE becomes
+// OUBLETRAY as so on. The first vowel of the original word becomes the starting of
+// the translation and proceeding letter being shifted towards the end and followed
+// by AY. Word that begins with a vowel is left unchanged. WAP to accept a word
+// and convert in to Piglatin word.
+
+public class string_10 {
+
+
+
+
+    public static void piglatin(String s){
+        int k = s.indexOf(s)
+    }
+
+
+    public static void main(String[] args)throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the string: ");
+        String s =sc.nextLine();
+    
+}
