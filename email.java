bcf50110a6b9e38574eb2859bfef2d5842b20b82
june0o7 <rajdeeppal6057@gmail.com>
2024-09-16 09:29:36 +0530

    email check incomplete

---
+++ email.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class email {
+    public static void emailcheck(String str){
+        
+    }
+    public static void main(String[] args) {
+        System.out.println("enter the email:  ");
+        Scanner sc = new Scanner(System.in);
+        String s =sc.nextLine() ;
+        emailcheck(s);
+    }
+    
+}
