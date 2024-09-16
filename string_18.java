66379bef76b50e74d66e1d3e44763509c1f81fb0
june0o7 <rajdeeppal6057@gmail.com>
2024-09-16 11:39:04 +0530

    all set

--- string_18.java
+++ string_18.java
@@ -3,15 +3,17 @@ import java.util.*;
 public class string_18 {
 
     public static String swap(String s){
+       
         char[] charArray = s.toCharArray();
 
-        // Swap the first and last characters
-        char temp = charArray[0];
-        charArray[0] = charArray[charArray.length - 1];
-        charArray[charArray.length - 1] = temp;
+        
+         
+    char temp = charArray[0];
+    charArray[0] = charArray[charArray.length - 1];
+         charArray[charArray.length - 1] = temp;
 
-        // Convert the character array back to a string and return
-        return new String(charArray);
+        
+ return new String(charArray);
     }
     public static void main(String[] args) {
         System.out.println("enter the string:  ");
